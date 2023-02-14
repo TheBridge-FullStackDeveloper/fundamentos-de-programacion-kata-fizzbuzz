5702277e4e89a1daa10e1390a6fa851a824cb850
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-02-14 19:38:53 +0100

    ===

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,15 @@
-for(let variable=0;variable<=1000,variable++){
-  return
-console.log(for())}
+for (let i = 1; i <= 1000; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    if (i % 5 === 0) {
+        console.log("Buzz")
+    }
+    if (i % 3 === 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+    }
+    else { console.log(i) }
+}
 
-  
+
+
