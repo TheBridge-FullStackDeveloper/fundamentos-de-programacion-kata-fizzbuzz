29e2360084079c5267a4b0484ac84be74245c60d
OmarjLand <omar.land2010@gmail.com>
2023-02-15 22:37:54 +0100

    Added the = simbols as said

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ for ( let i=0; i<=1000; i++ ) {
 
     if( i % 3 == 0 && i % 5 == 0 ){
         console.log( "FizzBuzz" );
-    } else if( i % 3 == 0 ) {
+    } else if( i % 3 === 0 ) {
         console.log( "Fizz" );
-    } else if ( i % 5 == 0 ) {
+    } else if ( i % 5 === 0 ) {
         console.log( "Buzz" );
     } else {
         console.log( i );
