7408befb9b2f467ef6b0110796e33d025d2bcb93
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-02-15 16:05:53 +0100

    correción

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,14 @@
-for (let i = 1; i <= 1000; i++) {
-    if (i % 3 === 0) {
-        console.log("Fizz")
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
     }
-    if (i % 5 === 0) {
-        console.log("Buzz")
+    else if (i % 3 === 0) {
+        console.log("Fizz");
     }
-    if (i % 3 === 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+    else if (i % 5 === 0) {
+        console.log("Buzz");
     }
     else { console.log(i) }
 }
-
-
 
